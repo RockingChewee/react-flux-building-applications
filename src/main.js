6bb28c3361a41cdb807870da56e94c845f7210f7
Aleksandr Fokin <aleksandr.fokin@gmail.com>
2017-05-30 01:39:49 +0300

    Creating React Components

--- src/main.js
+++ src/main.js
@@ -1,5 +1,38 @@
+//"use strict"; // Cannot 'use strict' here due to the need of having the jquery variable in a global namespace. This is workarounded by using IIFE.
 $ = jQuery = require('jquery'); // Bootstrap expects jquery to be in a global namespace
 
-var App = console.log('Hello world from Browserify');
+var React = require('react');
+var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
+var Header = require('./components/common/header');
 
-module.exports = App;
+(function(win) { // an IIFE to workaround the 'use strict' and jquery global variable collision
+
+  "use strict";
+
+  var App = React.createClass({
+    render: function() {
+      var Child;
+      switch(this.props.route) { // capturing the attribute values passed over by the parent component (this.props)
+        case 'about': Child = About; break;
+        default: Child = Home;
+      }
+
+      return (
+        <div>
+          <Header/>
+          <Child/>
+        </div>
+      );
+    }
+  });
+
+  function render() {
+    var route = win.location.hash.substr(1); // 'window' to 'win', since 'win' is passed to the IIFE
+    React.render(<App route={route} />, document.getElementById('app')); // passing props to the child component
+  }
+
+  win.addEventListener('hashchange', render); // Event that occurres when there is a hash change in the URL, e.g. http://localhost:9005/#about
+  render();
+
+})(window);
