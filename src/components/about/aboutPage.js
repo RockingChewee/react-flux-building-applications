73df114eac165f20f4627297a927ecd82cf95b12
Aleksandr Fokin <aleksandr.fokin@gmail.com>
2017-06-03 14:24:47 +0300

    React Router

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -3,6 +3,20 @@
 var React = require('react');
 
 var Home = React.createClass({
+  statics: {
+    willTransitionTo: function(transition, params, query, callback) {
+      if (!confirm('Are you sure you want to read a page that\'s this boring?')) {
+        transition.abort(); // this is fired once unless the page is reloaded...
+      } else {
+        callback();
+      }
+    },
+    willTransitionFrom: function(transition, component) {
+      if (!confirm('Are you sure you want to leave a page that\'s this exciting?')) {
+        transition.abort();
+      }
+    }
+  },
   render: function() {
     return (
       <div className="jumbotron">
---
+++ src/routes.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var React = require('react');
+
+var Router = require('react-router');
+var DefaultRoute = Router.DefaultRoute;
+var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
+var Redirect = Router.Redirect;
+
+var routes = (
+  <Route name="app" path="/" handler={require('./components/app')}> // this is shown for each page
+    <DefaultRoute handler={require('./components/homePage')}/> // if chosen, this is shown together with the root route as a sub-route
+    <Route name="authors" handler={require('./components/authors/authorPage')} />
+    // 'name' is the defined name of the route that is to be used in the code while referring to the route.
+    // 'path' is the part of URI that corresponds to the route. It is optional attribute that can be ommitted if it matches the 'name'.
+    <Route name="about" handler={require('./components/about/aboutPage')} />
+    <NotFoundRoute handler={require('./components/notFoundPage')}/>
+    <Redirect from="about-us" to="about" /> // if the URL was renamed
+    <Redirect from="awthurs" to="authors" /> // to handle potential typos if needed
+    <Redirect from="about/*" to="about" /> 'from' can also contain special characters, like '*'
+  </Route>
+);
+
+module.exports = routes;
